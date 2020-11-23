a429e8c9418573803312a9acf626fb99ad0f0845
Emanuel Huber <emanuel.tesv@gmail.com>
2020-11-23 19:49:09 -0300

    Update train.sh

--- experiments/utils/train.sh
+++ experiments/utils/train.sh
@@ -103,9 +103,9 @@ fi
 cp -f $CONFIG $OUTPUT/model_config.json
 
 if [[ ! ${Predict,,} = 'true' ]]; then
-  CMD=" --do_train"
+  CMD=" --do_train --do_eval --do_predict"
 else
-  CMD="--do_eval --do_predict" 
+  CMD=" --do_train --do_eval --do_predict" 
 fi
 
 parameters="--task_name $Task $CMD \
